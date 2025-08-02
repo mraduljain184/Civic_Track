a66e653239243aeaa7bc8055ee48c6a17380af3a
Mradul Jain <mraduljain184@gmail.com>
2025-08-02 12:30:10 +0530

    Basic changes

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRouter = require('./Routes/AuthRouter');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRouter = require("./Routes/AuthRouter");
 const app = express();
-require('./Models/db'); // Ensure the database connection is established
+require("./Models/db"); // Ensure the database connection is established
 
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors({
-  origin: 'http://localhost:5174',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
+app.use("/auth", authRouter);
 
-app.use('/auth', authRouter);
-
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Civic Track API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Civic Track API!");
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
